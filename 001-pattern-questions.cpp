6a2228eec708f58c06dee3d6d0499a52c167ee4c
Mohammad Salah <thesalahrand@gmail.com>
2023-09-19 11:30:56 +0600

    feat: all 22 patterns solved

--- 001-pattern-questions.cpp
+++ 001-pattern-questions.cpp
@@ -100,7 +100,7 @@ void pattern10(int n) {
   }
 }
 
-void pattern11(int n) { 
+void pattern11(int n) {
   for(int i = 0; i < n; i++) {
     int start_dig = i % 2 ? 0 : 1;
     for(int j = 0; j <= i; j++) {
@@ -111,7 +111,7 @@ void pattern11(int n) {
   }
 }
 
-void pattern12(int n) { 
+void pattern12(int n) {
   for(int i = 0; i < n; i++) {
     for(int j = 1; j <= i + 1; j++) {
       cout << j;
@@ -126,7 +126,7 @@ void pattern12(int n) {
   }
 }
 
-void pattern13(int n) { 
+void pattern13(int n) {
   for(int i = 0; i < n; i++) {
     for(int j = 0, num = (i * (i + 1) / 2) + 1; j <= i; j++, num++) {
       cout << num << ' ';
@@ -135,7 +135,7 @@ void pattern13(int n) {
   }
 }
 
-void pattern14(int n) { 
+void pattern14(int n) {
   for(int i = 0; i < n; i++) {
     for(char ch = 'A'; ch <= 'A' + i; ch++) {
       cout << ch << ' ';
@@ -144,7 +144,7 @@ void pattern14(int n) {
   }
 }
 
-void pattern15(int n) { 
+void pattern15(int n) {
   for(int i = 0; i < n; i++) {
     for(char ch = 'A'; ch <= 'A' + (n - 1 - i); ch++) {
       cout << ch << ' ';
@@ -153,7 +153,7 @@ void pattern15(int n) {
   }
 }
 
-void pattern16(int n) { 
+void pattern16(int n) {
   for(int i = 0; i < n; i++) {
     for(int j = 0; j <= i; j++) {
       cout << (char) ('A' + i);
@@ -162,7 +162,7 @@ void pattern16(int n) {
   }
 }
 
-void pattern17(int n) { 
+void pattern17(int n) {
   for(int i = 0; i < n; i++) {
     for(int j = 0; j < n - i - 1; j++) {
       cout << ' ';
@@ -180,11 +180,71 @@ void pattern17(int n) {
   }
 }
 
-void pattern18(int n) { 
+void pattern18(int n) {
   for(int i = 0; i < n; i++) {
     char ch = 'A' + (n - i - 1);
     for(int j = 0; j <= i; j++, ch++) {
       cout << ch << ' ';
+    }
+    cout << '\n';
+  }
+}
+
+void pattern19(int n) {
+  for(int i = 0; i < 2 * n; i++) {
+    int star_cnt = (i < n) ? n - i : i - n + 1;
+    int space_cnt = (i < n) ? 2 * i : (2 * n) - ((i - n + 1) * 2);
+
+    for(int j = 0; j < star_cnt; j++) {
+      cout << '*';
+    }
+    for(int j = 0; j < space_cnt; j++) {
+      cout << ' ';
+    }
+    for(int j = 0; j < star_cnt; j++) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
+}
+
+void pattern20(int n) {
+  for(int i = 0; i < (2 * n) - 1; i++) {
+    int star_cnt = (i < n) ? i + 1 : n - (i - n + 1);
+    int space_cnt = (i < n) ? (2 * n) - (2 * (i + 1)) : 2 * (i - n + 1);
+   
+    for(int j = 0; j < star_cnt; j++) {
+      cout << '*';
+    }
+    for(int j = 0; j < space_cnt; j++) {
+      cout << ' ';
+    }
+    for(int j = 0; j < star_cnt; j++) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
+}
+
+void pattern21(int n) {
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      char ch = (i == 0 || j == 0 || i == n - 1 || j == n - 1) ? '*' : ' ';
+      cout << ch;
+    }
+    cout << '\n';
+  }
+}
+
+void pattern22(int n) {
+  for(int i = 0; i < 2 * n - 1; i++) {
+    for(int j = 0; j < 2 * n - 1; j++) {
+      int top_distance = i;
+      int left_distance = j;
+      int right_distance = (2 * n - 2) - i;
+      int bottom_distance = (2 * n - 2) - j;
+      int min_distance = min(top_distance, min(left_distance, min(right_distance, bottom_distance)));
+      cout << n - min_distance << ' ';
     }
     cout << '\n';
   }
@@ -199,7 +259,7 @@ int main() {
   while (t--) {
     int n;
     cin >> n;
-    pattern18(n);
+    pattern22(n);
     cout << '\n';
   }
 
